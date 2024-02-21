666d75032916b8467ecc24f448a5133e44890ed7
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2024-02-21 22:04:25 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statisctics/Statistics';
+import { useState } from 'react';
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+	const [value, setValue] = useState({
+		good: 0,
+		neutral: 0,
+		bad: 0,
+	});
+	const countTotalFeedback = () => {
+		const { good, neutral, bad } = value;
+		return good + neutral + bad;
+	};
+	const countPositiveFeedbackPercentage = () => {
+		return Math.round((value.good / countTotalFeedback()) * 100);
+	};
+	const onLeaveFeedback = event => {
+		const option = event.target.textContent.toLowerCase();
+		setValue(prevState => ({
+			...prevState,
+			[option]: prevState[option] + 1,
+		}));
+	};
+	const {good, neutral, bad} = value;
+	return (
+		<div>
+			<Section title="Please leave your feedback">
+				<FeedbackOptions options={Object.keys(value)} onLeaveFeedback={onLeaveFeedback} />
+			</Section>
+			<Section title="Statistics">
+				{countTotalFeedback() ? (
+					<Statistics
+						good={good}
+						neutral={neutral}
+						bad={bad}
+						total={countTotalFeedback}
+						positivePercentarge={`${countPositiveFeedbackPercentage()}%`}
+					/>
+				) : (
+					'No feedback'
+				)}
+			</Section>
+		</div>
+	);
 };
